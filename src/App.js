6be4a6dd29ab76dcdc055faefa46592949249b23
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-04-07 20:54:32 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './images/blue-logo.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img 
+            src={logo} 
+            alt="Bristol Code School"
+            className="main-logo vw-100" />
         </header>
+        <section className="strapline background-pink">
+          <h1 className="f1 tc blue">
+            Bristol based coding tution, tailored to your needs
+          </h1>
+        </section>
+        <section className="enrol">
+          <button className="yellow tc">Book your place now!</button>
+        </section>
+        <section>
+          About
+        </section>
+        <section>
+          syllabus
+        </section>
+        <section>
+          enrol again
+        </section>
       </div>
     );
   }
