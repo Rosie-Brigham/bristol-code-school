a2c50aff344310da7e5882cf4f56aa7435170aae
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-04-28 15:43:58 +0100

    Updates

---
+++ precache-manifest.ca0d688ef69b7a3fd5599cf14e80ed8e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "672a2094a68a00039fba11b8469f2239",
+    "url": "/bristol-code-school/static/media/hot-air-balloon.672a2094.jpg"
+  },
+  {
+    "revision": "b65ad6ec7254fabbda1e",
+    "url": "/bristol-code-school/static/css/main.6ae5666a.chunk.css"
+  },
+  {
+    "revision": "e60c1c8d482036277b87",
+    "url": "/bristol-code-school/static/js/runtime~main.26b508c8.js"
+  },
+  {
+    "revision": "0154f9689bcca81074f4",
+    "url": "/bristol-code-school/static/js/2.be2527d6.chunk.js"
+  },
+  {
+    "revision": "9bcf6e80f44ee1fdb22e06bacdabb761",
+    "url": "/bristol-code-school/static/media/blue-logo.9bcf6e80.png"
+  },
+  {
+    "revision": "b65ad6ec7254fabbda1e",
+    "url": "/bristol-code-school/static/js/main.3451efba.chunk.js"
+  },
+  {
+    "revision": "538e31c86af80f01d5b2114fac4df5b5",
+    "url": "/bristol-code-school/static/media/balloon-taking-off.538e31c8.jpg"
+  },
+  {
+    "revision": "ab606dbe648fa9ab3f34d5e44bd877cf",
+    "url": "/bristol-code-school/static/media/bristol-bridge.ab606dbe.jpg"
+  },
+  {
+    "revision": "02fbd7729bd7da1969c4b9de22abfd3b",
+    "url": "/bristol-code-school/static/media/balloon-fire.02fbd772.jpg"
+  },
+  {
+    "revision": "30c5f3015ca28f2ae0299d41467bbf53",
+    "url": "/bristol-code-school/static/media/blue-balloon.30c5f301.jpg"
+  },
+  {
+    "revision": "81b7792cab059c2be3b030ac3a80398b",
+    "url": "/bristol-code-school/index.html"
+  }
+];
