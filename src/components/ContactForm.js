5e4902bd73cc2082adf4f1eda52c87e142c118f8
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-04-28 18:17:06 +0100

    corrects form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,21 +5,16 @@ class ContactForm extends Component {
     return (
       <section>
         <h2 className="f1 center tc pink background-blue ma0 pv5">Questions or queries? Give us a bell!</h2>
-        <div className="background-blue">
-          <form action="https://formspree.io/rosiebrigham@gmail.com" method="POST" />
-          <form className="w-40 center flex flex-wrap">
-            
+        <div className="background-blue" id="contact">
+          <form action="https://formspree.io/rosiebrigham@gmail.com" method="POST">            
             <input className="w-100 pa2 ma3" type="text" id="name" name="firstname" placeholder="Your name"/>
 
             <input className="w-100 pa2 ma3" type="text" id="email" name="lastname" placeholder="Your email address"/>
 
             <input className="w-100 pa2 ma3 h3" type="text" id="lname" name="lastname" placeholder="Fill me with questions!"/>
 
-            <input className="submit center ph5 pv3  mv5" type="submit" value="Submit"/>
-
+            <button type="submit" className="submit center ph5 pv3  mv5">Submit</button>
           </form>
-
-
         </div>
       </section>
     )
