4f60529d91355f71f68528e6e50c9ff87524a4cb
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-04-28 14:45:32 +0100

    refactors out why learn section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import WhyLearn from './components/WhyLearn'
+
 import logo from './images/blue-logo.png'
-import balloon from './images/hot-air-balloon.jpg'
-import ballonThree from './images/balloon3.jpg'
-import blueBalloon from './images/blue-balloon.jpg'
-// import codeImage from './images-codimage.jpg'
 
 class App extends Component {
   render() {
@@ -48,42 +46,8 @@ class App extends Component {
         </div> */}
       </section>
 
-      <section className="why-learn vh-100 background-blue">
-        <h2 className="center tc ma0 pt6 pb5 w-100 pink">Why learn to Code?</h2>
-
-        <div className="mh6 why-one">
-          <h3 className="grey f1 mt0">Stand out from the crowd</h3>
-          <p className="grey w-60 f2 lh-copy ">
-            A basic knowledge of programming is crucial to the modern workplace. It is the extra string to your bow that is crucial in small creative companies where your job encompasses a little bit of everything.
-          </p>
-        </div>
-
-        <div className="why-two">
-          <div className="fr tr w-60">
-            <h3 className="grey f1 mt0">Understand other aspects of tech</h3>
-            <p className="grey f2 lh-copy">
-              Knowledge of process of programming will mean you can learn other aspects of how tech works quicker. Learning how to code doesn't have to lead into being a programmer, but gaining a digital literacy that can be invaluble. 
-            </p>
-          </div>
-        </div>
-
-        <div className="why-three">
-          <h3 className="grey f1 mt0">Empower yourself</h3>
-          <p className="grey f2 w-50 lh-copy">
-            Have hundered of ideas the next big thing? Learn to code so you can make that dream your reality without hving to rely on someone else. 
-          </p>
-        </div>
-
-        <div className="why-four">
-          <div className="fr tr w-60">
-            <h3 className="pink f1 mt0">You can do it</h3>
-            <p className="grey f2 lh-copy">
-              Coding isn't a club only for elite mathematicians, but a set of skills that anyone can pick up with time and patience, just like any other skill
-            </p>
-          </div>
-        </div>
-        
-      </section>
+      <WhyLearn/>
+      
 
       <section className="background-light-blue flex flex-wrap pb6 w-100">
         <h2 className="center tc ma0 pv4 w-100 pink">What you will learn</h2>
---
+++ src/components/WhyLearn.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+// import logo from './images/blue-logo.png'
+
+class WhyLearn extends Component {
+  render() {
+    return (
+      <section className="why-learn  background-blue">
+        <h2 className="center tc ma0 pt4 pb3 w-100 pink background-blue">Why learn to Code?</h2>
+
+        <section className="why-learn-scroll pt5 vh-100 ">
+
+        
+          <div className="mh6 why-one">
+            <h3 className="grey f1 mt0">Stand out from the crowd</h3>
+            <p className="grey w-60 f2 lh-copy ">
+              A basic knowledge of programming is crucial to the modern workplace. It is the extra string to your bow that is crucial in small creative companies where your job encompasses a little bit of everything.
+            </p>
+          </div>
+
+          <div className="why-two">
+            <div className="fr tr w-60">
+              <h3 className="grey f1 mt0">Understand other aspects of tech</h3>
+              <p className="grey f2 lh-copy">
+                Knowledge of process of programming will mean you can learn other aspects of how tech works quicker. Learning how to code doesn't have to lead into being a programmer, but gaining a digital literacy that can be invaluble. 
+              </p>
+            </div>
+          </div>
+
+          <div className="why-three">
+            <h3 className="grey f1 mt0">Empower yourself</h3>
+            <p className="grey f2 w-50 lh-copy">
+              Have hundered of ideas the next big thing? Learn to code so you can make that dream your reality without hving to rely on someone else. 
+            </p>
+          </div>
+
+          <div className="why-four">
+            <div className="fr tr w-60">
+              <h3 className="pink f1 mt0">You can do it</h3>
+              <p className="grey f2 lh-copy">
+                Coding isn't a club only for elite mathematicians, but something that anyone can pick up with time and patience, just like any other skill
+              </p>
+            </div>
+          </div>
+        </section>
+        
+      </section>
+    )
+  }
+}
+
+export default WhyLearn;
