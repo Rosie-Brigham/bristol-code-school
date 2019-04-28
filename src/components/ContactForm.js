2d83ce0961d2041660635183fc4d7651aa6ff6ce
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-04-28 16:57:33 +0100

    Adds form spree to contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,15 +6,20 @@ class ContactForm extends Component {
       <section>
         <h2 className="f1 center tc pink background-blue ma0 pv5">Questions or queries? Give us a bell!</h2>
         <div className="background-blue">
+          <form action="https://formspree.io/rosiebrigham@gmail.com" method="POST" />
           <form className="w-40 center flex flex-wrap">
+            
             <input className="w-100 pa2 ma3" type="text" id="name" name="firstname" placeholder="Your name"/>
 
             <input className="w-100 pa2 ma3" type="text" id="email" name="lastname" placeholder="Your email address"/>
 
             <input className="w-100 pa2 ma3 h3" type="text" id="lname" name="lastname" placeholder="Fill me with questions!"/>
-          
+
             <input className="submit center ph5 pv3  mv5" type="submit" value="Submit"/>
+
           </form>
+
+
         </div>
       </section>
     )
