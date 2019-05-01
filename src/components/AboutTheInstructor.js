42d0c7719910783ac5aa1b59d030b7065405d471
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-05-01 08:58:53 +0100

    More mobile friendly design

--- src/components/AboutTheInstructor.js
+++ src/components/AboutTheInstructor.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 class AboutTheInstructor extends Component {
   render() {
     return (
-      <section className="background-dpink ph6 pv5 flex flex-wrap">
-        <div className="w-20">
+      <section id="#who" className="background-dpink ph3 ph6-ns pv5 flex flex-column-reverse flex-wrap-ns">
+        <div className="center w-80 w-20-ns">
           <img 
             src="https://rosie-brigham.github.io/images/mecodesquare.jpg" 
             className="br-100" 
           />
         </div>
-        <div className="w-60 ph5 lh-copy">
+        <div className="w-100 w-60-ns ph3 ph5-ns lh-copy">
           <p className="grey">
             Rosie first learnt to code in 2013 after graduating from History of Art as a devout technophobe. She discovered that programming and 'computer stuff' didn't have to be scary and inaccessible, but rather fun and empowering.
             </p>
@@ -18,7 +18,7 @@ class AboutTheInstructor extends Component {
             She has been working as a programmer and teaching people how to code ever since. She has previously worked with educational companies such as Decoded, Code First: Girls, Superhi and Enspiral Dev Academy before setting up Bristol Code School.
           </p>
         </div>
-        <div className="w-20 grey">
+        <div className="w-100 w-20-ns tc grey">
           <h2 className="f1">About the Instructor</h2>
         </div>
       </section>
--- src/components/WhyLearn.js
+++ src/components/WhyLearn.js
@@ -6,37 +6,37 @@ import React, { Component } from 'react';
 class WhyLearn extends Component {
   render() {
     return (
-      <section className="why-learn  background-blue">
-        <h2 className="center tc ma0 pt4 pb3 w-100 pink background-blue">Why learn to Code?</h2>
+      <section id="#why" className="why-learn  background-blue">
+        <h2 className="center tc f2 f0-ns ma0 pt4 pb3 w-100 pink background-blue">Why learn to Code?</h2>
 
         <section className="why-learn-scroll pt5 h-100 ">  
           <div className="mh6 why-one">
-            <h3 className="grey f1 mt0">Stand out from the crowd</h3>
-            <p className="grey w-60 f2 lh-copy ">
+            <h3 className="white f1 mt0">Stand out from the crowd</h3>
+            <p className="grey w-100 w-60-ns f4 f2-ns  lh-copy ">
               A basic knowledge of programming is crucial to the modern workplace. It is the extra string to your bow that is crucial in small creative companies where your job encompasses a little bit of everything.
             </p>
           </div>
 
           <div className="why-two">
-            <div className="fr tr w-60">
-              <h3 className="grey f1 mt0">Understand other aspects of tech</h3>
-              <p className="grey f2 lh-copy">
+            <div className="fr tr w-100 w-60-ns">
+              <h3 className="white f1 mt0 w-80 w-100-ns ml4">Understand other aspects of tech</h3>
+              <p className="grey f4 f2-ns lh-copy">
                 Knowledge of programming processes will enable you to engage in other concepts that underpin the tech you encounter everyday. Learning how to code doesn't have to lead into being a programmer, but improving digital literacy can be invaluble. 
               </p>
             </div>
           </div>
 
           <div className="why-three">
-            <h3 className="grey f1 mt0">Empower yourself</h3>
-            <p className="grey f2 w-50 lh-copy">
+            <h3 className="white f1 mt0">Empower yourself</h3>
+            <p className="grey f4 f2-ns w-100 w-50-nslh-copy">
               Have hundred of ideas the next big thing? Learn to code so you can make that dream your reality without having to rely on someone else. 
             </p>
           </div>
 
           <div className="why-four">
-            <div className="fr tr w-60">
+            <div className="fr tr w-100 w-60-ns">
               <h3 className="pink f1 mt0">You can do it</h3>
-              <p className="grey f2 lh-copy">
+              <p className="grey f4 f2-ns lh-copy">
                 Coding isn't a club only for elite mathematicians, but something that anyone can pick up with time and patience, just like any other skill
               </p>
             </div>
